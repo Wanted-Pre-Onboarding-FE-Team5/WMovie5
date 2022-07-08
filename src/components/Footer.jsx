bf8a9dc88e04a0baf1385f8b4ead0abeb3348d70
ymStudyLog <ylee585@gmail.com>
2022-07-08 16:17:53 +0900

    Feat: Footer 컴포넌트 생성

---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styled from "styled-components";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterText>WMovie5 v.0.1.0</FooterText>
+      <FooterText>created by wanted-pre-onboarding-fe-5th team5</FooterText>
+      <FooterText>
+        participants : 최보성, 김연진, 이유미, 홍정민, 김슬기
+      </FooterText>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
+
+const FooterContainer = styled.div`
+  width: 100%;
+  height: 70px;
+  margin: 0;
+  padding: 30px 0;
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const FooterText = styled.span`
+  margin: 5px 0;
+  text-align: center;
+  font-size: 0.8rem;
+`;
