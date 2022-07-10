290ec0f8f30cdc1cad79d420a7d78272bd5f88f8
sgsg9447 <sgsg9447@gmail.com>
2022-07-11 02:55:43 +0900

    Modify: MovieList 기능 구현 및  각 페이지 연결

--- src/App.js
+++ src/App.js
@@ -8,15 +8,7 @@ import Favorites from "./pages/Favorites";
 import GNB from "./components/GNB";
 
 function App() {
-  const { movies, getMovies, patchMovieById } = useMovieModel();
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  const onClickImageCallback = (id, data) => {
-    patchMovieById(id, data).then(getMovies);
-  };
+  const { movies } = useMovieModel();
 
   return (
     <GlobalWrapper>
@@ -27,19 +19,6 @@ function App() {
           <Route path="/search" element={<Search movies={movies} />} />
           <Route path="/favorites" element={<Favorites movies={movies} />} />
         </Routes>
-
-        {/* 아랫 부분은 멘토님 코드입니다(영화 리스트 랜더링 코드). */}
-        {movies?.map((movie, index) => (
-          <span
-            key={index}
-            style={{ border: movie.like ? "10px solid blue" : "none" }}
-            onClick={() =>
-              onClickImageCallback(movie.id, { like: !movie.like })
-            }
-          >
-            <img src={movie.medium_cover_image} alt="poster" />
-          </span>
-        ))}
       </BrowserRouter>
     </GlobalWrapper>
   );
@@ -48,5 +27,5 @@ export default App;
 
 const GlobalWrapper = styled.div`
   width: 100%;
-  min-height: 100%;
+  min-height: 100vh;
 `;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import MovieList from "../components/MovieList";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <MainContainer>
+      <MovieList />
+    </MainContainer>
+  );
+};
 
-export default Home
+export default Home;
+
+const MainContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #191b22;
+`;
