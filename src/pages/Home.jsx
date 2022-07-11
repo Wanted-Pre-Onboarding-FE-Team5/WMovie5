28a938c14cb2a103a972c5808c673c22fa87db0d
qhtjd2131 <qhtjd2131@gmail.com>
2022-07-11 14:37:57 +0900

    Feat : Recoil 적용, useMovieModel 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useRecoilState } from "recoil";
+import { useMovieModel } from "../models/useMovieModel";
+import { movieState } from "../state/atoms";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [movies, setMovies] = useRecoilState(movieState);
+  const { toggleFavoriteById, getMovies } = useMovieModel();
 
-export default Home
+  const onClickHandler = async (id, data) => {
+    await toggleFavoriteById(id, data);
+    await getMovies().then((response) => {
+      setMovies(response);
+    });
+  };
+
+  return (
+    <div>
+      {/* <div>
+        {movies?.map((d, index) => (
+          <div
+            key={index}
+            onClick={() => {
+              onClickHandler(d.id, { like: !d.like });
+            }}
+            style={d.like ? { border: "3px solid blue" } : {}}
+          >
+            {d.medium_cover_image}
+          </div>
+        ))}
+      
+      </div> */}
+    </div>
+  );
+};
+
+export default Home;
